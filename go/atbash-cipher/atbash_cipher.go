7613ed09b5ae292402aa644d4d037110ec808675
Cindy Cheung <cindycheung1408@gmail.com>
2021-12-27 11:09:51 +0000

    atbash-cipher

--- go/atbash-cipher/atbash_cipher.go
+++ go/atbash-cipher/atbash_cipher.go
@@ -1,5 +1,47 @@
 package atbash
 
+import (
+	"regexp"
+	"strconv"
+	"strings"
+)
+
 func Atbash(s string) string {
-	panic("Please implement the Atbash function")
+	cipher := make(map[string]string)
+	result := []string{}
+	alphabet := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+	for i := 0; i < len(alphabet); i++ {
+		cipher[alphabet[i]] = alphabet[len(alphabet)-1-i]
+	}
+	r := regexp.MustCompile(`[a-zA-Z0-9]`)
+	filtered := r.FindAllString(s, -1)
+	for _, char := range filtered {
+		_, err := strconv.Atoi(char)
+		isNum := false
+		if err == nil {
+			isNum = true
+		}
+		for k, v := range cipher {
+			if strings.ToLower(char) == k && !isNum {
+				result = append(result, v)
+			}
+		}
+		if isNum {
+			result = append(result, char)
+		}
+	}
+	i := 0
+	resultBreak := []string{}
+	for _, char := range result {
+		if i == 5 {
+			resultBreak = append(resultBreak, " ")
+			i = 0
+			resultBreak = append(resultBreak, char)
+			i++
+		} else {
+			resultBreak = append(resultBreak, char)
+			i++
+		}
+	}
+	return strings.Join(resultBreak, "")
 }
