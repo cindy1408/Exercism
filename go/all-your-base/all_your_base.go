4209123863e7c486d7ee5fbc4eebaf22fe044f19
Cindy Cheung <cindycheung1408@gmail.com>
2021-12-29 15:23:00 +0000

    all your base

--- go/all-your-base/all_your_base.go
+++ go/all-your-base/all_your_base.go
@@ -1,24 +1,41 @@
 package allyourbase
 
 import (
-	"fmt"
+	"errors"
 	"math"
 )
+
 func ConvertToBase(inputBase int, inputDigits []int, outputBase int) ([]int, error) {
 	if inputBase < 2 || outputBase < 2 {
-		return nil, fmt.Errorf("there is an issue with %d or %d",inputBase, outputBase)
+		if inputBase < 2 {
+			return nil, errors.New("input base must be >= 2")
+		} else {
+			return nil, errors.New("output base must be >= 2")
+		}
 	}
 	results := []int{}
 	for i, value := range inputDigits {
-		if value > inputBase && value < 0 {
-			return nil, fmt.Errorf("value %d must be less than input base and more than or equal to 0", value)
+		if value >= inputBase || value < 0 {
+			// fmt.Println("HERE")
+			return nil, errors.New("all digits must satisfy 0 <= d < input base")
 		}
-		result := math.Pow(float64(inputBase), float64(len(inputDigits)-i))
-		results = append(results, value * int(result))
+		inputBaseResult := math.Pow(float64(inputBase), float64(len(inputDigits)-i-1))
+		results = append(results, value*int(inputBaseResult))
 	}
-	return results, nil
+	totalNum := 0
+	for _, n := range results {
+		totalNum += n
+	}
+	outputDigits := []int{}
+	for totalNum > 0 {
+		outputDigits = append([]int{totalNum % outputBase}, outputDigits...)
+		totalNum /= outputBase
+	}
+
+	if len(outputDigits) == 0 {
+		outputDigits = append(outputDigits, 0)
+	}
+	return outputDigits, nil
 }
-
-
 
 // all digits must satisfy 0 <= d < input base
