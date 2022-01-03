74e823614964def6d000ef70a41a2584774666eb
Cindy Cheung <cindycheung1408@gmail.com>
2022-01-03 12:45:53 +0000

    sieve

--- go/sieve/sieve.go
+++ go/sieve/sieve.go
@@ -1,5 +1,18 @@
 package sieve
 
 func Sieve(limit int) []int {
-	panic("Please implement the Sieve function")
+	primeNums := []int{}
+	factors := 0
+	for i := 2; i <= limit; i++ {
+		for j := 2; j <= i; j++ {
+			if i%j == 0 {
+				factors = factors + 1
+			}
+		}
+		if factors == 1 {
+			primeNums = append(primeNums, i)
+		}
+		factors = 0
+	}
+	return primeNums
 }
