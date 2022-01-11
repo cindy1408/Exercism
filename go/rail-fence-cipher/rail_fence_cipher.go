97ed726b1e43d7c2eced18d4359f08b52fe4c96a
Cindy Cheung <cindycheung1408@gmail.com>
2022-01-11 21:12:25 +0000

    draft

--- go/rail-fence-cipher/rail_fence_cipher.go
+++ go/rail-fence-cipher/rail_fence_cipher.go
@@ -6,46 +6,38 @@ import (
 )
 
 func Encode(message string, rails int) string {
-
-	stringMap := map[int][]string{}
 	messageArr := strings.Split(message, "")
-	directionUp := false 
-	countj := 1
-	countk := rails
-	for i:=0; i<len(messageArr)-2; i++{
-		if countj == 1 || countj == rails-1 || countk == rails || countk == 1 {
-			directionUp = !directionUp
-		}
-		fmt.Println(directionUp)
-		if !directionUp {
-			for j:=countj; j<rails; j++ {
-				fmt.Println("1: ", countj)
-				stringMap[countj] = append(stringMap[countj], messageArr[i+j])
-				// if countj == rails-1 {
-				// 	countj = 1
-				// } else {
-				// 	countj +=1
-				// }
+	downRail := true 
+	startRail := 0 
+	endRail := rails-1
+	resultMap := map[int][]string{}
+	for _, v := range messageArr {
+		if downRail {
+			// fmt.Println(startRail, v)
+			resultMap[startRail] = append(resultMap[startRail], v)
+			startRail = startRail + 1
+			if startRail == endRail {
+				startRail = 0 
+				downRail = false
 			}
 		} else {
-			for k:=countk; k>1; k-- {
-				fmt.Println("2: ", countk)
-				stringMap[countk] = append(stringMap[countk], messageArr[i+k-1])
-				// if countk == 2 {
-				// 	countj = rails
-				// } else {
-				// 	countk -=1
-				// }
+			// fmt.Println(endRail, v)
+			resultMap[endRail] = append(resultMap[endRail], v)
+			endRail = endRail - 1
+			if endRail == 0 {
+				endRail = rails-1 
+				downRail = true
 			}
 		}
+	
 	}
-	result := []string{}
-	for _, v := range stringMap {
-		result = append(result, v...)
+	resultArr := []string{}
+	for _, v := range resultMap {
+		joined := strings.Join(v, "")
+		resultArr = append(resultArr, joined)
 	}
-	fmt.Println(stringMap)
-	fmt.Println(strings.Join(result, ""))
-	return strings.Join(result, "")
+	fmt.Println(strings.Join(resultArr, ""))
+	return strings.Join(resultArr, "")
 }
 
 func Decode(message string, rails int) string {
