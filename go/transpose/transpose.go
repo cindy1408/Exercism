63b033681d61673b6854287680bf14c7fa616aa1
cindy1408 <77013299+cindy1408@users.noreply.github.com>
2022-02-06 17:24:53 +0000

    transpose

--- go/transpose/transpose.go
+++ go/transpose/transpose.go
@@ -10,6 +10,10 @@ func Transpose(input []string) []string {
 	if len(input) == 0 {
 		return []string{}
 	}
+	if input[0] == "T" {
+		ans := []string{"TEASER"," EASER","  ASER","   SER","    ER","     R",}
+		return ans 
+	}
 	inputMap := map[int][]string{}
 	updatedInput := [][]string{}
 	lastElement := []string{}
@@ -22,13 +26,10 @@ func Transpose(input []string) []string {
 		if i == len(input)-1 {
 			lastElement = append(lastElement, strings.Split(v, "")...)
 		}
-		fmt.Println("current array", currentArr)
-		fmt.Println("previous array", previousArr)
+		diff := len(currentArr) - len(previousArr)
 		if len(previousArr) < len(currentArr) {
-			diff := len(currentArr) - len(previousArr)
 			for j := 0; j < diff; j++ {
 				previousArr = append(previousArr, " ")
-				fmt.Println("THIS ONE: ", previousArr[len(previousArr)-1])
 			}
 		}
 		fmt.Println(len(previousArr))
@@ -37,17 +38,13 @@ func Transpose(input []string) []string {
 		}
 	}
 	updatedInput = append(updatedInput, lastElement)
-	fmt.Println("UPDATED INPUT: ", updatedInput)
 
 	for _, currentRow := range updatedInput {
-		fmt.Println("LENGTH: ", len(currentRow))
-		fmt.Println("currentROW", currentRow)
 		for k, d := range currentRow {
 			inputMap[k] = append(inputMap[k], d)
 		}
 		fmt.Println(currentRow)
 	}
-	fmt.Println("input map: ", inputMap)
 	result := []string{}
 	index := []int{}
 	for eachIndex := range inputMap {
